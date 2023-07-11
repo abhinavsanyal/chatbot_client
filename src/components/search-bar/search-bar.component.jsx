072793f87e3f8aba13282f652a4be41138cf6b73
Gaurang Bhardwaj <gaurang.bhardwaj@sleek.com>
2023-07-11 09:33:03 +0530

    ✨ text to text implementation

--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -1,17 +1,34 @@
-import { useEffect } from "react";
-import { Container, InputAdornment, TextField } from "@mui/material";
+import { Container, InputAdornment, TextField, form } from "@mui/material";
+import { setChatData, setIsFetchingAnswers } from "reducers/chat-slice";
 import { useState } from "react";
 import LinearProgress from "@mui/material/LinearProgress";
 import SearchIcon from "@mui/icons-material/Search";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RecordMessage } from "./record-message.component";
 
+import { getTextToTextCompletion } from "../../api";
+
 export const SearchBar = () => {
+  const dispatch = useDispatch();
   const chatStore = useSelector((state) => state.chat);
 
   const [searchTerm, setSearchTerm] = useState("");
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const getTextToTextChats = async (event) => {
+    event.preventDefault();
+    try {
+      dispatch(setIsFetchingAnswers(true));
+      const res = await getTextToTextCompletion(searchTerm);
+      dispatch(setChatData(res));
+      setSearchTerm("");
+    } catch (error) {
+      console.log("Unable to get text answer : ", error);
+    } finally {
+      dispatch(setIsFetchingAnswers(false));
+    }
   };
 
   return (
@@ -29,20 +46,22 @@ export const SearchBar = () => {
           mb: 2,
         }}
       >
-        <TextField
-          fullWidth
-          id="search"
-          type="search"
-          value={searchTerm}
-          onChange={handleChange}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
+        <form style={{ width: "100%" }} onSubmit={getTextToTextChats}>
+          <TextField
+            fullWidth
+            id="search"
+            type="search"
+            value={searchTerm}
+            onChange={handleChange}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </form>
         <RecordMessage />
       </Container>
     </>
